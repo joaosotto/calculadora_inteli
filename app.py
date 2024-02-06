57393e6c5e2415934504df42dbdeffc051eefe75
joaosotto <joao.maior@sou.inteli.edu.br>
2024-02-06 19:41:56 -0300

    feat: ask for two numbers and  a operation

---
+++ app.py
@@ -0,0 +1,7 @@
+first_number = input('Enter first number: \n \n ')
+second_number = input('Enter second number: \n \n ')
+operation = input('Wich one do you want? \n1 - Sum\n 2 - Sub\n 3 - Multp\n 4 - Div')
+
+
+
+
