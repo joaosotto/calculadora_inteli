fcf0be481e4aa0d4b88f731493ac93479b68f706
joaosotto <joao.maior@sou.inteli.edu.br>
2024-02-06 20:09:49 -0300

    feat: add a condicional structure to 'operation' variable
    condicional structure to know wich operation to perform

--- app.py
+++ app.py
@@ -14,5 +14,17 @@ def multp(num1, num2):
 def div(num1, num2):
     return num1 / num2
 
+if operation == '1': 
+    print(sum(first_number, second_number))
 
+elif operation == '2':
+    print(sub(first_number, second_number))
 
+elif operation == '3':
+    print(multp(first_number, second_number))
+    
+elif operation == '4':
+    print(div(first_number, second_number))
+
+else: 
+    print('Sorry, did not uderstand you.')
