2e90745378cee04f6bd7f17e3c2d487bcd42645b
joaosotto <joao.maior@sou.inteli.edu.br>
2024-02-07 10:27:54 -0300

    feat: add a main function to reset after an operation
    call main function after print result. If invalid operation, shut down

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
-first_number = int(input('Enter first number: \n'))
-second_number = int(input('Enter second number: \n'))
-chosen_operation = int(input('Wich one do you want? \n1 - Sum\n2 - Sub\n3 - Multp\n4 - Div\n'))
+def main():    
+    first_number = int(input('Enter first number: \n'))
+    second_number = int(input('Enter second number: \n'))
+    chosen_operation = int(input('Wich one do you want? \n1 - Sum\n2 - Sub\n3 - Multp\n4 - Div\n'))
 
-def sum(num1, num2):
-    return num1 + num2
+    def sum(num1, num2):
+        return num1 + num2
 
-def sub(num1, num2):
-    return num1 - num2
+    def sub(num1, num2):
+        return num1 - num2
 
-def multp(num1, num2):
-    return num1 * num2
+    def multp(num1, num2):
+        return num1 * num2
 
-def div(num1, num2):
-    return num1 / num2
+    def div(num1, num2):
+        return num1 / num2
 
-operations = {
-    1: sum,
-    2: sub,
-    3: multp,
-    4: div
-}
+    operations = {
+        1: sum,
+        2: sub,
+        3: multp,
+        4: div
+    }
 
-if chosen_operation in operations:
-    result = operations[chosen_operation](first_number, second_number)
-    print(result)
-    
-else: 
-    print('Sorry, did not uderstand you. Shutting down...')
+    if chosen_operation in operations:
+        result = operations[chosen_operation](first_number, second_number)
+        print()
+        print(result)
+        print()
+        main()
+        
+    else: 
+        print('Sorry, did not uderstand you. Shutting down...')
+
+main()
