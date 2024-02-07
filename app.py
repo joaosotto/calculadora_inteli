95e53375dfa5385b8b64c9f3e359101244c009f4
joaosotto <joao.maior@sou.inteli.edu.br>
2024-02-07 11:50:38 -0300

    feat: add new operation (potentiation)

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 def main():    
     first_number = int(input('Enter first number: \n'))
     second_number = int(input('Enter second number: \n'))
-    chosen_operation = int(input('Wich one do you want? \n1 - Sum\n2 - Sub\n3 - Multp\n4 - Div\n'))
+    chosen_operation = int(input('Wich one do you want? \n1 - Sum\n2 - Subtract\n3 - Multiply\n4 - Divide\n5 - Potentiation\n'))
 
     def sum(num1, num2):
         return num1 + num2
@@ -14,18 +14,22 @@ def main():
 
     def div(num1, num2):
         return num1 / num2
+    
+    def potentiation(num1, num2):
+        return num1 ** num2
 
     operations = {
         1: sum,
         2: sub,
         3: multp,
-        4: div
+        4: div,
+        5: potentiation
     }
 
     if chosen_operation in operations:
         result = operations[chosen_operation](first_number, second_number)
         print()
-        print(result)
+        print('Result: '+result)
         print()
         main()
         
